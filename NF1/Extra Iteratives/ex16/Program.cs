9255e54e4e444442ddb661f7d465f72cbbd8a062
Jordi Fons <jordi.fons@institutmontilivi.cat>
2024-11-25 14:52:58 +0100

    afegim ex16

---
+++ NF1/Extra Iteratives/ex16/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int n, m, factN = 1, factM = 1, factNM = 1;
+            double resultat;
+
+            //Demanar valors entrada
+            Console.Write("Escriu n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escriu m: ");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            if (n < m)
+            {
+                int aux = m;
+                m = n;
+                n = aux;
+            }
+
+            if (n > m && n > 1 && m > 0)
+            {
+                for (int i = 1; i <= n; i++)
+                    factN *= i;
+                for (int i = 1; i <= m; i++)
+                    factM *= i;
+                for (int i = 1; i <= n - m; i++)
+                    factNM *= i;
+
+                resultat = (double)factN/(factM*factNM);
+                Console.WriteLine(resultat);
+            }
+
+        }
+
+    }
+}
---
+++ NF1/Extra Iteratives/ex16b/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex16b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, m = 2;
+            double resultat = Factorial(n) / (Factorial(m) * Factorial(n - m));
+        }
+        static int Factorial(int num)
+        {
+            int res = 1;
+            for (int i = 1; i <= num; i++)
+                res *= i;
+            return res;
+        }
+    }
+}
